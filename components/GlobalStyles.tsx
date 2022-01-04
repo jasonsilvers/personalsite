48f70942870764d1cd3f19e76ff85072c090b2c3
Jason Silvers <jason.silvers.2@us.af.mil>
2022-01-04 10:51:19 -1000

    fix build error

--- components/GlobalStyles.tsx
+++ components/GlobalStyles.tsx
@@ -1,16 +1,15 @@
-import { createGlobalStyle } from 'styled-components'
-import tw, { theme, GlobalStyles as BaseStyles } from 'twin.macro'
+import { createGlobalStyle } from 'styled-components';
+import tw, { theme, GlobalStyles as BaseStyles } from 'twin.macro';
 
 const CustomStyles = createGlobalStyle`
   body {
-    -webkit-tap-highlight-color: ${theme`colors.purple.500`};
     ${tw`antialiased`}
   }
-`
+`;
 
 const GlobalStyles = () => (
   <>
     <BaseStyles />
     <CustomStyles />
   </>
-)
+);
---
+++ tailwind.config.js
@@ -0,0 +1,1 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
