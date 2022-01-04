57e77c5e30b5c4071f959e73d59ec5b4b43b906d
Jason Silvers <jason.silvers.2@us.af.mil>
2022-01-04 10:53:43 -1000

    fix global style

--- components/GlobalStyles.tsx
+++ components/GlobalStyles.tsx
@@ -1,11 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
 import tw, { theme, GlobalStyles as BaseStyles } from 'twin.macro';
 
-const CustomStyles = createGlobalStyle`
-  body {
-    ${tw`antialiased`}
-  }
-`;
+const CustomStyles = createGlobalStyle({ // < Object styles
+  body: {
+    WebkitTapHighlightColor: theme`colors.purple.500`,
+    ...tw`antialiased`,
+  },
+})
 
 const GlobalStyles = () => (
   <>
