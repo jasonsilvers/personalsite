bb9d18240533eddb5fc0875bad1ded23fcf96189
Jason Silvers <jason.silvers.2@us.af.mil>
2022-01-12 11:50:15 -1000

    Allow sentry to run only in production

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -9,7 +9,8 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 Sentry.init({
   dsn: SENTRY_DSN || 'https://60b7778d85424cd6ae0fbc52e0993723@o1114949.ingest.sentry.io/6146468',
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1.0
+  tracesSampleRate: 1.0,
+  environment: 'production'
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
