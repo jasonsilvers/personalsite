3a7185191b545a598725abc0e0a3c7ef51c2da6c
Jason Silvers <jason.silvers.2@us.af.mil>
2022-01-12 10:15:50 -1000

    add deploy github action, added 1st cypress test

---
+++ cypress/integration/main.spec.ts
@@ -0,0 +1,15 @@
+describe('main', () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+
+    console.log()
+    cy.visit('/')
+  })
+
+  it('should display welcome message', () => {
+    cy.findByText(/welcome/i).should('exist')
+  })
+})
